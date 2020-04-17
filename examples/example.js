162f3b9321580e8d99ffa43a591d7a4484f6cec6
Jake Cyr <cyrjake@gmail.com>
2020-04-17 19:30:35 -0400

    Fix typo

--- examples/example.js
+++ examples/example.js
@@ -6,7 +6,7 @@ const values = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
 // create new instance of NodeScaler class
 const scaler = new NodeScaler();
 
-// scale all values betwee 0 and 1
+// scale all values between 0 and 1
 const scaledValues = scaler.scaleValues(values, 0, 1);
 
 console.log(scaledValues.values); // [ 0, 0.11, 0.22, 0.33, 0.44, 0.55, 0.66, 0.77, 0.88, 1 ]
